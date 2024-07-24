1ec5dd4651b00b2a8adfdb71d16cb6ec659d1d7a
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-07-24 18:08:05 +0300

    Remove blank line in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ buildscript {
         classpath(libs.navigation.safe.args.gradle.plugin)
     }
 }
-
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
