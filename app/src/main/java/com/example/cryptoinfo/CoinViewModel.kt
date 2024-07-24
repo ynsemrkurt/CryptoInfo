c349c5c0c08890a3961d79c4665cb7628a870de2
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-07-24 18:09:45 +0300

    Fetch coin data and chart data from API using coroutines

---
+++ app/src/main/java/com/example/cryptoinfo/CoinViewModel.kt
@@ -0,0 +1,53 @@
+package com.example.cryptoinfo
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class CoinViewModel : ViewModel() {
+
+    private val _coins = MutableLiveData<List<Coin>>()
+    val coins: LiveData<List<Coin>> get() = _coins
+
+    private val _chartData = MutableLiveData<Map<String, List<Pair<Float, Float>>>>()
+    val chartData: LiveData<Map<String, List<Pair<Float, Float>>>> get() = _chartData
+
+    private val _error = MutableLiveData<String>()
+    val error: LiveData<String> get() = _error
+
+    init {
+        fetchCoins()
+    }
+
+    private fun fetchCoins() {
+        viewModelScope.launch {
+            try {
+                val coinList = RetrofitInstance.coinApiService.getCoins("usd",5)
+                _coins.postValue(coinList)
+                fetchAndDisplayAllCharts(coinList.map { it.id })
+            } catch (e: Exception) {
+                _error.value = e.message
+            }
+        }
+    }
+
+    fun fetchAndDisplayAllCharts(coinIds: List<String>) {
+        viewModelScope.launch {
+            try {
+                val charts = mutableMapOf<String, List<Pair<Float, Float>>>()
+                for (coinId in coinIds) {
+                    val response = RetrofitInstance.coinApiService.getMarketChart(coinId, "usd", "1")
+                    val chartDataList = response.prices.map {
+                        Pair(it[0].toFloat(), it[1].toFloat())
+                    }
+                    charts[coinId] = chartDataList
+                }
+                _chartData.postValue(charts)
+            } catch (e: Exception) {
+                _error.value = e.message
+            }
+        }
+    }
+}
