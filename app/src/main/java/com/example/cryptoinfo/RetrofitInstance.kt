a97ace4ddd308ab4303eefcae6a243619d453118
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-07-24 18:11:03 +0300

    Add Retrofit instance for networking

---
+++ app/src/main/java/com/example/cryptoinfo/RetrofitInstance.kt
@@ -0,0 +1,29 @@
+package com.example.cryptoinfo
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private const val BASE_URL = "https://api.coingecko.com/api/v3/"
+    private const val API_KEY = "CG-4m5NyFPDGM3665dSMGadbJFV"
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor { chain ->
+            val request = chain.request().newBuilder()
+                .addHeader("accept", "application/json")
+                .addHeader("x-cg-demo-api-key", API_KEY)
+                .build()
+            chain.proceed(request)
+        }
+        .build()
+
+    val coinApiService: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}
