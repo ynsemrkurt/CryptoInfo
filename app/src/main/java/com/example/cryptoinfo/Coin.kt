91c68f98b1e9880be737e92649a36b0cbc7e7272
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-07-25 13:28:14 +0300

    Implement Parcelable interface for Coin and MarketChartResponse classes

--- app/src/main/java/com/example/cryptoinfo/Coin.kt
+++ app/src/main/java/com/example/cryptoinfo/Coin.kt
@@ -1,7 +1,10 @@
 package com.example.cryptoinfo
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Coin(
     val id: String,
     val symbol: String,
@@ -17,8 +20,17 @@ data class Coin(
     val low24h: Double,
     @SerializedName("price_change_24h")
     val priceChange24h: Double,
-)
+    @SerializedName("circulating_supply")
+    val circulatingSupply : Double,
+    @SerializedName("market_cap")
+    val marketCap: Long,
+    @SerializedName("market_cap_rank")
+    val marketCapRank: Int,
+    @SerializedName("total_volume")
+    val totalVolume: Long,
+): Parcelable
 
+@Parcelize
 data class MarketChartResponse(
     val prices: List<List<Double>>
-)
+):Parcelable
