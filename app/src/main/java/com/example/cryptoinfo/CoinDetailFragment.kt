bb45d0eddba33fe1b4216c9263d7596825810173
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-07-25 13:29:27 +0300

    Add CoinDetailFragment

---
+++ app/src/main/java/com/example/cryptoinfo/CoinDetailFragment.kt
@@ -0,0 +1,17 @@
+package com.example.cryptoinfo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CoinDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_coin_detail, container, false)
+    }
+}
