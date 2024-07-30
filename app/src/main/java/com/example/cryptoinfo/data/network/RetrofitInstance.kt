260ecb8cc1b75b0da6ecaae913f9eb8c771da25e
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-07-30 10:09:18 +0300

    Set connection, read, and write timeouts for Retrofit client

--- app/src/main/java/com/example/cryptoinfo/data/network/RetrofitInstance.kt
+++ app/src/main/java/com/example/cryptoinfo/data/network/RetrofitInstance.kt
@@ -3,12 +3,16 @@ package com.example.cryptoinfo.data.network
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitInstance {
     private const val BASE_URL = "https://api.coingecko.com/api/v3/"
     private const val API_KEY = "CG-4m5NyFPDGM3665dSMGadbJFV"
 
     private val client = OkHttpClient.Builder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
         .addInterceptor { chain ->
             val request = chain.request().newBuilder()
                 .addHeader("accept", "application/json")
